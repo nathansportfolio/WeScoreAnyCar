7ff4ff17acf13927d5f722c8df2c318a89bf1090
nathansportfolio <nathan.doore@gmail.com>
2022-02-02 21:52:50 +0000

    New Stuff

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ab60c9791b7036c5"; }
+/******/ 		__webpack_require__.h = function() { return "933667abfa2bcab3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- utils/useLocalStorage.tsx
+++ utils/useLocalStorage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-function useLocalStorage(key, initialValue) {
+function useLocalStorage(key:any, initialValue:any) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
@@ -17,7 +17,7 @@ function useLocalStorage(key, initialValue) {
   });
   // Return a wrapped version of useState's setter function that ...
   // ... persists the new value to localStorage.
-  const setValue = (value) => {
+  const setValue = (value:any) => {
     try {
       // Allow value to be a function so we have same API as useState
       const valueToStore =
